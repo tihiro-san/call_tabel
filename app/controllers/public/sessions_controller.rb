8eacb9212d82477aba55d05a363b2beab7989f70
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-12-12 16:09:38 +0000

    [Add]devise adminとpublicの機能追加

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+  # 退会しているかを判断するメソッド
+  def user_state
+  ## 【処理内容1】 入力されたemailからアカウントを1件取得  
+    @user = User.find_by(email: params[:customer][:email])
+  ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@user
+  ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別  
+    if @user.valid_password?(params[:customer][:password]) && (@user.is_deleted == true)
+      ## 【処理内容3】
+      redirect_to new_user_session_path, notice: "退会済みです"
+    end
+  end 
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admins, controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+  # 顧客用
+# URL /customers/sign_in ...
+  devise_for :users, [:registrations, :passwords] ,controllers: {
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
