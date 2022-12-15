ab71951244cf3472fd8b55da8674430f5ceb42db
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-12-15 00:17:41 +0000

    [Update]contacts_controllerの記述を追加

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,13 +1,40 @@
 class Public::ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def edit
+    @contact = Contact.find(params[:id])
   end
-
+  
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      redirect_to contact_path(@contact)
+    else
+      render edit_contact_path(@contact)
+    end
+  
+  end
+  
   def new
+    @contact = Contact.new
+  end
+  
+  def create
+    if @contact = Contact.new(contact_params)
+      redirect_to user_oath(@contact)
+    else
+      render 'new'
+    end
+  end  
+  
+  private
+  def contact_params
+    params.require(:contact).permit(:organization_name, :contact_number, :contact_postcode, :contact_address, :contact_status, :counterparty_post, :counterparty_name, :employees, :website, :remarks)
   end
 end
