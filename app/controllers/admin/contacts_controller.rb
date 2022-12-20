a3142eef808e6b2ec2c12580778e7ad34f348610
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2022-12-20 05:05:56 +0000

    [Add]管理者側連絡先一覧画面の追加

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,13 +1,41 @@
 class Admin::ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 
   def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      redirect_to contact_path(@contact)
+    else
+      render edit_contact_path(@contact)
+    end
+
   end
 
   def new
+    @contact = Contact.new
+  end
+
+  def create
+    if @contact = Contact.create(contact_params)
+      ContactManager.create(user_id: current_user.id, contact_id: @contact.id)
+      redirect_to contact_path(@contact)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:organization_name, :contact_number, :contact_postcode, :contact_address, :contact_status, :counterparty_post, :counterparty_name, :employees, :website, :remarks)
   end
 end
